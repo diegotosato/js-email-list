e3a00d67250575b699cf61cad75be493660c1947
diegotosato <tosato1998@gmail.com>
2025-10-23 14:08:07 +0200

    write pseudo code

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 */
+
+//PSEUDO CODE
+/*
+# create an empty list in html
+# assign an ID to the list
+# isolate into variable with getElementById
+# create a function to create markup to add list items
+    - into the markup insert variable for emails to generate
+# isolate the endpoint into a variable
+# fetch API -> then: parsing/then: data/catch: err
+# into second then
+    - destructuring API object to isolate the mail creation
+# initialize for loop
+    - loop 10 times
+    - variable isolate by id += API mail creation
+*/
