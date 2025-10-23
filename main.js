8bd01761132092f4bc69f5702ae60d5d904a318e
diegotosato <tosato1998@gmail.com>
2025-10-23 14:16:33 +0200

    destracture data abject into response to isolate the random email generator

--- main.js
+++ main.js
@@ -49,7 +49,12 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 fetch(endpoint)
 .then(response => response.json())
 .then(data => {
+
     console.log(data);
+    
+    const {response} = data
+    console.log(response);
+    
     
 })
 .catch(error => {
